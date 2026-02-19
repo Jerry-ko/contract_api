944cff9d8f93485b6ccb623506d88713cee5ac46
hannah <mjko8903@gmail.com>
2026-02-19 21:24:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server ok");
+});
+
+app.listen(3000, () => {
+  console.log("server running");
+});
