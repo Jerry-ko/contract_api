cf107ce7ee62cc655bba2495f0bdcf7d659b534d
hannah <mjko8903@gmail.com>
2026-02-22 17:00:41 +0900

    multer 설치

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import multer from "multer";
 
 const app = express();
+const upload = multer({ dest: "uploads/" });
 
-app.get("/", (req, res) => {
-  res.send("server ok");
+app.post("/api/analyze", upload.single("file"), (req, res) => {
+  console.log("파일 옴?", !!req.file);
+  console.log(req.file);
+  res.json({ ok: true });
 });
 
 app.listen(3000, () => {
